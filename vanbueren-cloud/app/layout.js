acfb2b43c24ec3f96845eefb66788873fd3304d9
Daniel Van Bueren <daniel.vanbueren@proton.me>
2026-07-10 11:03:15 -0500

    Add icons

--- vanbueren-cloud/app/layout.js
+++ vanbueren-cloud/app/layout.js
@@ -8,6 +8,18 @@ import Providers from './providers'
 export const metadata = {
   title: 'vanbueren.cloud - Dashboard',
   description: 'Dashboard',
+  icons: {
+    icon: [
+      { url: '/icon/icon0.svg', type: 'image/svg+xml' },
+      { url: '/icon/icon1.png', type: 'image/png' },
+    ],
+    shortcut: '/icon/favicon.ico',
+    apple: '/icon/apple-icon.png',
+  },
+  manifest: '/icon/manifest.json',
+  appleWebApp: {
+    title: 'vanbueren.cloud',
+  },
 }
 
 export default function RootLayout({ children }) {
