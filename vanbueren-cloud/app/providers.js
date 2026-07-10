7d0aecfb5fe04d9000af87abb0b43e917681674d
Daniel Van Bueren <daniel.vanbueren@proton.me>
2026-07-10 10:08:11 -0500

    Add Vercel analytics

--- vanbueren-cloud/app/providers.js
+++ vanbueren-cloud/app/providers.js
@@ -1,5 +1,6 @@
 'use client'
 
+import { Analytics } from "@vercel/analytics/next"
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from './theme'
@@ -7,6 +8,7 @@ import theme from './theme'
 export default function Providers({ children }) {
   return (
     <ThemeProvider theme={theme}>
+      <Analytics />
       <CssBaseline />
       {children}
     </ThemeProvider>
